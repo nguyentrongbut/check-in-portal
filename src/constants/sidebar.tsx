2745ce3b4c6447e4a22cf879094e8f64b3d39345
nguyentrongbut <cloly031128@gmail.com>
2025-07-29 00:29:26 +0700

    feat: add sidebar and responsive. Ref #2

---
+++ src/constants/sidebar.tsx
@@ -0,0 +1,17 @@
+import {Calendar, Gift, Home, Plus, Shield, Users, Wallet} from "lucide-react";
+import {TNavSideBar} from "@/types/layout";
+
+export const allocatorNavItems: TNavSideBar[] = [
+    { href: "/dashboard", label: "Dashboard", icon: Home },
+    { href: "/campaigns", label: "View All Campaigns", icon: Calendar },
+    { href: "/campaigns/create", label: "Create New Campaign", icon: Plus },
+    { href: "/vouchers", label: "Vouchers", icon: Gift },
+    { href: "/wallet", label: "Wallet / Point Balance", icon: Wallet },
+]
+
+export const adminNavItems: TNavSideBar[] = [
+    { href: "/admin/dashboard", label: "Dashboard", icon: Home },
+    { href: "/admin/campaigns", label: "Review Campaigns", icon: Calendar },
+    { href: "/admin/fraud", label: "Fraud Detection", icon: Shield },
+    { href: "/admin/users", label: "User Management", icon: Users },
+]
