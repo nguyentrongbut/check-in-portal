9a3cf2843c42b963a7a1e60ddddc7f42a10cfbcd
nguyentrongbut <cloly031128@gmail.com>
2025-08-01 21:14:26 +0700

    feat(ui): display list card balance. Ref #16

---
+++ src/components/pages/wallet/columns.history.transaction.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+import {ColumnDef} from "@tanstack/react-table"
+
+import {ArrowUpDown} from "lucide-react";
+import {TTransaction} from "@/types/data";
+
+export const columnsHistoryTransaction: ColumnDef<TTransaction>[] = [
+
+]
